4d66cc46e8eb6c0e6b2ec52a21e26c8920b0039f
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-04-22 14:05:11 -0400

    beans

--- app/src/main/java/com/nix/skales/commands/SkalesCommand.java
+++ app/src/main/java/com/nix/skales/commands/SkalesCommand.java
@@ -15,13 +15,13 @@ public class SkalesCommand implements commandExecutor{
                 String message = "&aSkales has one command, but this command has many possible arguments.\n\n&7/skales help\n&7/skales info\n&7/skales reload\n";
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&' message);}
             else if (args[0].equals("info")){
-                String message = "&aPlaceholder message"
+                String message = "&aPlaceholder message";
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&' message);}
             else if (args[0].equals("reload")){
-                String message = "&aPlaceholder message"
+                String message = "&aPlaceholder message";
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&' message);}}
         else {
-            String message = "&aYou must enter arguments with the command!\n\n&7/skales help\n&7/skales info\n&7/skales reload\n"
+            String message = "&aYou must enter arguments with the command!\n\n&7/skales help\n&7/skales info\n&7/skales reload\n";
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&' message);
         }
     }

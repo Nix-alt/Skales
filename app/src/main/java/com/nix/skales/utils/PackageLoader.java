f44cf53869c5410bc614d37b4ccc884ddcbdfda3
Nix-alt <167569462+Nix-alt@users.noreply.github.com>
2024-04-22 09:28:29 -0400

    9fujf

--- app/src/main/java/com/nix/skales/utils/PackageLoader.java
+++ app/src/main/java/com/nix/skales/utils/PackageLoader.java
@@ -1,18 +1,7 @@
 package com.nix.skales.utils;
 
-import ch.njol.skript.Skript;
-import ch.njol.skript.log.ErrorQuality;
-import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.LinkedList;
@@ -47,31 +36,3 @@ public class PackageLoader<T> {
     public CompletableFuture<LinkedList<T>> getList() {
         return instancesList;
     }
-
-    private static final String PREFIX = "&7[&aSkales&7] ";
-    private static final String PREFIX_ERROR = "&7[&aSkales &cERROR&7] ";
-    private static final Pattern HEX_PATTERN = Pattern.compile("<#([A-Fa-f\\d]){6}>");
-    private static final boolean SKRIPT_IS_THERE = Bukkit.getPluginManager().getPlugin("Skript") != null;
-
-    public static String getColString(String string) {
-        Matcher matcher = HEX_PATTERN.matcher(string);
-        if (SKRIPT_IS_THERE) {
-            while (matcher.find()) {
-                final ChatColor hexColor = ChatColor.of(matcher.group().substring(1, matcher.group().length() - 1));
-                final String before = string.substring(0, matcher.start());
-                final String after = string.substring(matcher.end());
-                string = before + hexColor + after;
-                matcher = HEX_PATTERN.matcher(string);
-            }
-        } else {
-            string = HEX_PATTERN.matcher(string).replaceAll("");
-        }
-        return ChatColor.translateAlternateColorCodes('&', string);
-    }
-
-
-    public static void log(String format, Object... objects) {
-        String log = String.format(format, objects);
-        Bukkit.getConsoleSender().sendMessage(getColString(PREFIX + log));
-    }
-}
